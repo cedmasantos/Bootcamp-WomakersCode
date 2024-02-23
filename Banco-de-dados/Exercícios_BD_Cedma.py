6b5e17abfca7ada6df1fc31e7f08b68d97f22dca
cedma.santos <cedma.santos@gmail.com>
2024-02-23 19:22:58 -0300

    Exercícios SQL - 5-7

--- Banco-de-dados/Exercícios_BD_Cedma.py
+++ Banco-de-dados/Exercícios_BD_Cedma.py
@@ -43,7 +43,7 @@ for alunos in dados3:
 
 dados4 = cursor.execute('SELECT COUNT(*) FROM alunos')
 myresult = dados4.fetchall()
-#print(myresult[-1][-1])
+print(myresult[-1][-1])
 
 # 4 Atualização e Remoção 
 
@@ -58,6 +58,63 @@ for alunos in dados5:
 dados6 = cursor.execute('DELETE FROM alunos WHERE id = 2')
 for alunos in dados6:
     print(alunos)
+
+# 5 Criar uma Tabela e Inserir Dados
+
+#cursor.execute('CREATE TABLE clientes(id INT PRIMARY KEY, nome VARCHAR(100), idade INT, saldo real);')
+
+#cursor.execute('INSERT INTO clientes(id, nome, idade, saldo) VALUES( 1, "Cedma", 37, 10)')
+#cursor.execute('INSERT INTO clientes(id, nome, idade, saldo) VALUES( 2, "Natalia", 25, 20)')
+#cursor.execute('INSERT INTO clientes(id, nome, idade, saldo) VALUES( 3, "Ana", 22, 30)')
+#cursor.execute('INSERT INTO clientes(id, nome, idade, saldo) VALUES( 4, "MAria", 20, 40)')
+#cursor.execute('INSERT INTO clientes(id, nome, idade, saldo) VALUES( 5, "Zoe", 30, 20)')
+
+# 6. Consultas e Funções Agregadas
+#a) Selecione o nome e a idade dos clientes com idade superior a 30 anos.
+
+dados7 = cursor.execute('SELECT nome, idade FROM clientes WHERE idade>30')
+myresult = dados7.fetchall()
+for clientes in myresult:
+    print(clientes)
+
+# b) Calcule o saldo médio dos clientes.
+
+
+dados8 = cursor.execute("SELECT AVG(saldo) FROM clientes")
+
+myresult  = dados8.fetchone()[0]
+
+print("Saldo médio dos clientes:", myresult)
+
+#) Encontre o cliente com o saldo máximo.
+
+dados9 =  cursor.execute("SELECT nome, idade, saldo FROM clientes WHERE saldo = (SELECT MAX(saldo) FROM clientes)")
+myresult = dados9.fetchall()
+for clientes in myresult:
+    print(clientes)
+
+# d) Conte quantos clientes têm saldo acima de 1000.
+
+
+dados10 = cursor.execute(("SELECT COUNT(*) FROM clientes WHERE saldo > 1000"))
+myresult = dados10.fetchall()
+print(myresult[-1][-1])
+
+# 7. Atualização e Remoção com Condições
+
+#a) Atualize o saldo de um cliente específico.
+
+dados11 = cursor.execute('UPDATE clientes SET saldo =1000 WHERE nome = "Cedma"')
+myresult = dados11.fetchall()
+for clientes in myresult:
+    print(clientes)
+
+#b) Remova um cliente pelo seu ID.
+
+dados12 = cursor.execute('DELETE FROM clientes WHERE id = 2')
+myresult = dados12.fetchall()
+for clientes in myresult:
+    print(clientes)
 
 
 #cursor.execute('CREATE TABLE produtos(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100));')
